a474e3500e03b0730c0a8ea83daa27601222da68
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2026-06-06 12:34:59 +0530

    WIP

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,62 @@
+import "./NavBar.css";
+export default function NavBar() {
+  let isSuperUser = true;
+  const menuArray = [
+    { menuItem: "IMG", link: "/favicon.png", visible: true, subMenu: false },
+    { menuItem: "Login", link: "#", visible: true, subMenu: false },
+    { menuItem: "Cheques", link: "#", visible: true, subMenu: false },
+    { menuItem: "File Movement", link: "#", visible: true, subMenu: false },
+    { menuItem: "Reports", link: "#", visible: true, subMenu: false },
+    {
+      menuItem: "Masters",
+      link: "#",
+      visible: true,
+      subMenu: [
+        { menuItem: "Files", link: "#", visible: true, subMenu: false },
+        { menuItem: "Schemes", link: "#", visible: true, subMenu: false },
+        {
+          menuItem: "Departments",
+          link: "#",
+          visible: isSuperUser,
+          subMenu: false,
+        },
+        { menuItem: "Users", link: "#", visible: true, subMenu: false },
+        {
+          menuItem: "Reset Password",
+          link: "#",
+          visible: isSuperUser,
+          subMenu: false,
+        },
+      ],
+    },
+    { menuItem: "About", link: "#", visible: true, subMenu: false },
+  ];
+  return (
+    <div className="nav-bar">
+      <ul className="nav-bar-list">
+        {menuArray.map((item) => (
+          <li key={item.menuItem.toLowerCase()}>
+            {/* 1. Ternary Operator & 2. Strict Equality Check (===) */}
+            {item.menuItem === "IMG" ? (
+              <img src={item.link} className="logo" alt="Logo" />
+            ) : (
+              <>
+                <a href={item.link}>{item.menuItem}</a>
+
+                {item.subMenu && (
+                  <ul className="sub-menu-list">
+                    {item.subMenu.map((subItem) => (
+                      <li key={subItem.menuItem.toLowerCase()}>
+                        <a href={subItem.link}>{subItem.menuItem}</a>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
